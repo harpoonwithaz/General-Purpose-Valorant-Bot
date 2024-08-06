c04ccf1eb6a508af05bd0167702a77f1ab3d0957
harpoonwithaz <otad19@gmail.com>
2024-08-06 15:42:28 -0700

    added comment

--- src/data_formatter.py
+++ src/data_formatter.py
@@ -1,3 +1,4 @@
+# Imports valorant API request module
 from api_modules import valorant_api
 
 def get_player_info(name: str, tag: str) -> dict:
@@ -6,10 +7,26 @@ def get_player_info(name: str, tag: str) -> dict:
 
     # Checks if nothing was returned from the API
     if basic_data != None:
+        # Makes request to API with PUUID to get detailed player data
         detailed_data, error_message = valorant_api.get_detailed_account_details(basic_data['data']['region'], basic_data['data']['puuid'])
-        print(detailed_data)
+        
+        # Checks if nothing was returned from the API
+        if detailed_data != None:
+            # Organizes JSON data from API into dictionary
+            player_data = {
+                'username': detailed_data['data']['name'],
+                'tagline': detailed_data['data']['tag'],
+                'PUUID': basic_data['data']['puuid'],
+                'region': basic_data['data']['region'],
+                'level': basic_data['data']['account_level'],
+                'banner_small': basic_data['data']['card']['small'],
+                'banner_large': basic_data['data']['card']['large'],
+                'banner_wide': basic_data['data']['card']['wide'],
+                'current_rank': detailed_data['data']['current_data']['currenttierpatched'],
+                'current_rank_tier': detailed_data['data']['current_data']['currenttierpatched']
+            }
     else:
         print('Error: ', error_message  )
     
-
+# This is a comment
 get_player_info('H00DBYAIR', 'BMWM5')
